532661ab245503f9d559cf2b56bbcefefee54bf7
Ahmad Magdy <magdy.a@hotmail.com>
2018-10-07 15:46:34 +0200

    Add examples in the stories/index.js

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,10 +4,11 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+// TODO Import your components here
 import { Button, Welcome } from '@storybook/react/demo';
 
+// TODO Add your own stories here
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
@@ -17,3 +18,12 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+/*
+    storiesOf('<name>', module)
+    .add('case1', () => <Component1 someprops={somevalues} />)
+
+    storiesOf('Button', module)
+    .add('default', () => <Button />)
+    .add('red color', () => <Button color="red" />)
+*/
