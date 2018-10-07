ad262d365d0fc101d2e5c9f203eebd8187e654b8
Ahmad Magdy <magdy.a@hotmail.com>
2018-10-07 15:52:22 +0200

    Split stories files into separate files

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,29 +1,5 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-// TODO Import your components here
-import { Button, Welcome } from '@storybook/react/demo';
-
-// TODO Add your own stories here
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
-
-/*
-    storiesOf('<name>', module)
-    .add('case1', () => <Component1 someprops={somevalues} />)
-
-    storiesOf('Button', module)
-    .add('default', () => <Button />)
-    .add('red color', () => <Button color="red" />)
-*/
+import './button'
+import './welcome'
+import './calendar'
