cbf647e7090daf719372fff88331b6b2c3164c76
Richard Paredes <chard.paredes@gmail.com>
2018-10-09 21:15:40 +0200

    removing <div> tags with styling on the mapcomponent.js storybook file

--- src/stories/mapcomponent.js
+++ src/stories/mapcomponent.js
@@ -4,35 +4,25 @@ import { storiesOf } from "@storybook/react";
 import MapComponent from "../components/map-component/MapComponent";
 
 storiesOf("MapComponent", module)
-  .add("Default setting", () => (
-    <div style={{ height: `600px`, width: `600px` }}>
-      <MapComponent />
-    </div>
-  ))
+  .add("Default setting", () => <MapComponent />)
   .add("set map center to HYF", () => (
-    <div style={{ height: `600px`, width: `600px` }}>
-      <MapComponent
-        mapCenter={{ lat: 55.6608889, lng: 12.5397036 }}
-        setMarker
-        Zoom={18}
-      />
-    </div>
+    <MapComponent
+      mapCenter={{ lat: 55.6608889, lng: 12.5397036 }}
+      setMarker
+      Zoom={18}
+    />
   ))
   .add("set map to UCPH Zoom=14", () => (
-    <div style={{ height: `600px`, width: `600px` }}>
-      <MapComponent
-        mapCenter={{ lat: 55.6802303, lng: 12.5718571 }}
-        setMarker
-        Zoom={14}
-      />
-    </div>
+    <MapComponent
+      mapCenter={{ lat: 55.6802303, lng: 12.5718571 }}
+      setMarker
+      Zoom={14}
+    />
   ))
   .add("set map to UCPH Zoom=17", () => (
-    <div style={{ height: `600px`, width: `600px` }}>
-      <MapComponent
-        mapCenter={{ lat: 55.6802303, lng: 12.5718571 }}
-        setMarker
-        Zoom={17}
-      />
-    </div>
+    <MapComponent
+      mapCenter={{ lat: 55.6802303, lng: 12.5718571 }}
+      setMarker
+      Zoom={17}
+    />
   ));
