6d0441ebc057b9d0afad78950e2e3c8e8d600d05
Mohammad Javad Zamani <jvdzmn@gmail.com>
2018-10-11 19:37:23 +0200

    add picture to sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import sidebar from './sidebar.css'
 import Flip from 'react-reveal/Flip';
 
 class SideBar extends React.Component {
@@ -6,8 +7,9 @@ class SideBar extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="sidebar">
         <Flip left>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Forsyth_Barr_Building%2C_Christchurch_02.JPG/800px-Forsyth_Barr_Building%2C_Christchurch_02.JPG" className="image"/>
         {this.props.text}
         </Flip>
       </div>
