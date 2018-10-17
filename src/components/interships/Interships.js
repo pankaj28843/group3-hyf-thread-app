649d7cb565140918a231915f974c65627bdbb6a4
Milamanieva <milamanieva@gmail.com>
2018-10-17 11:56:11 +0200

    Router

---
+++ src/components/interships/Interships.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../header/Header"
+
+class Internships extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <li>Internships</li>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Internships;
---
+++ src/components/mentors/Mentors.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../header/Header"
+
+class Mentors extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <li>Mentors</li>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Mentors;
