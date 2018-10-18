11ce60cf3efe3b5441f807d4b4d4394f63a90d29
Majd Darraj <majd.darraj@hotmail.com>
2018-10-18 18:53:55 +0200

    Internships page component DONE | Styling

--- src/components/cardsList/InternshipsList.js
+++ src/components/cardsList/InternshipsList.js
@@ -24,10 +24,29 @@ class InternshipsList extends Component {
       .then(parsedJSON =>
         parsedJSON.map(internship => ({
           id: `${internship.id}`,
-          title: `${internship.internship_title}`,
-          category: `${internship.internship_category}`,
+          internship_title: `${internship.internship_title}`,
+          internship_category: `${internship.internship_category}`,
           department: `${internship.department}`,
-          description: `${internship.internship_description}`
+          internship_description: `${internship.internship_description}`,
+          organisation_description: `${internship.organisation_description}`,
+          internship_agreement: `${internship.internship_agreement}`,
+          internship_requirements: `${internship.internship_requirements}`,
+          application_requirements: `${internship.application_requirements}`,
+          internship_availabiltiy_schedule: `${
+            internship.internship_availabiltiy_schedule
+          }`,
+          travel_expenses: `${internship.travel_expenses}`,
+          location: `${internship.location}`,
+          internship_add_date: `${internship.internship_add_date}`,
+          closing_date: `${internship.closing_date}`,
+          internship_deadline: `${internship.internship_deadline}`,
+          contact_person: `${internship.contact_person}`,
+          phone_contact: `${internship.phone_contact}`,
+          email_contact: `${internship.email_contact}`,
+          organisation_address: `${internship.organisation_address}`,
+          organisation_website: `${internship.organisation_website}`,
+          internship_theme_image: `${internship.internship_theme_image}`,
+          active: `${internship.active}`
         }))
       )
       .then(internshipsData =>
@@ -53,19 +72,58 @@ class InternshipsList extends Component {
               ? internshipsData.map(internship => {
                   const {
                     id,
-                    title,
-                    category,
+                    internship_title,
+                    internship_category,
+                    organisation_name,
                     department,
-                    description
+                    internship_description,
+                    organisation_description,
+                    internship_agreement,
+                    internship_requirements,
+                    application_requirements,
+                    internship_availabiltiy_schedule,
+                    travel_expenses,
+                    location,
+                    internship_add_date,
+                    closing_date,
+                    internship_deadline,
+                    contact_person,
+                    phone_contact,
+                    email_contact,
+                    organisation_address,
+                    organisation_website,
+                    internship_theme_image,
+                    active
                   } = internship;
+
                   return (
                     <InternshipCard
-                      title={title}
+                      internship_title={internship_title}
                       id={id}
                       key={id}
                       department={department}
-                      category={category}
-                      organisation_description={description}
+                      internship_category={internship_category}
+                      organisation_name={organisation_name}
+                      internship_description={internship_description}
+                      organisation_description={organisation_description}
+                      internship_agreement={internship_agreement}
+                      internship_requirements={internship_requirements}
+                      application_requirements={application_requirements}
+                      internship_availabiltiy_schedule={
+                        internship_availabiltiy_schedule
+                      }
+                      travel_expenses={travel_expenses}
+                      location={location}
+                      internship_add_date={internship_add_date}
+                      closing_date={closing_date}
+                      internship_deadline={internship_deadline}
+                      contact_person={contact_person}
+                      phone_contact={phone_contact}
+                      email_contact={email_contact}
+                      organisation_address={organisation_address}
+                      organisation_website={organisation_website}
+                      internship_theme_image={internship_theme_image}
+                      active={active}
                     />
                   );
                 })
